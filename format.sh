70d5b243c0fabb874280e2ca48e922371464885b
shunak <shuna966@gmail.com>
2021-03-12 12:12:57 +0900

    updated

--- format.sh
+++ format.sh
@@ -1,1 +1,7 @@
 cat "$1".json | jq -c '.[]' > "$1"-prcd.json
+sed -i -e '2,3d' "$1".json
+sed -i -e '352,361d' "$1".json
+cat "$1".json | jq -c '.[]' > "$1"-prcd.json
+if [ -e "$1".json-e ]; then
+	rm "$1".json-e
+fi
